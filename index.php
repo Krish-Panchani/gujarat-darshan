82eeae48b7782423a3da4e091f3fe7f74bac9825
Krish-Panchani <krishpanchani1346@gmail.com>
2024-06-17 12:20:17 +0530

    chore: Add Swiper vertical slide carousel to homepage

--- index.php
+++ index.php
@@ -1,6 +1,48 @@
 <?php require_once "_header.php"; ?>
 <?php require_once "_navbar.php"; ?>
 
+<div class="w-full relative px-6">
+      <div class="swiper vertical-slide-carousel swiper-container relative h-96">
+      <div class="swiper-wrapper">
+       <div class="swiper-slide">
+         <div class="bg1 bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+           <span class="text-8xl font-bold text-white">Welcome to Gujarat Darshan</span>
+         </div>
+       </div>
+       <div class="swiper-slide">
+         <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+           <span class="text-3xl font-semibold text-indigo-600">Slide 2 </span>
+         </div>
+       </div>
+       <div class="swiper-slide">
+         <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+           <span class="text-3xl font-semibold text-indigo-600">Slide 3 </span>
+         </div>
+       </div>
+      </div>
+      <div class="swiper-pagination text-orange-500"></div>
+      </div>
+      </div>
+      </div>
 
+
+<script>
+    var swiper = new Swiper(".vertical-slide-carousel", {
+        loop: true,
+        direction: 'vertical',
+        mousewheelControl: true,
+        mousewheel: {
+            releaseOnEdges: true,
+        },
+        spaceBetween: 30,
+        grabCursor: true,
+        pagination: {
+            el: ".vertical-slide-carousel .swiper-pagination",
+            clickable: true,
+        },
+    });
+</script>
+
+<!-- <script src="/js/index.js"></script> -->
 
 <?php require_once "_footer.php"; ?>
