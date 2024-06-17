df24ea228a15157b70a44511cd10bcc06b0de4d2
Krish-Panchani <krishpanchani1346@gmail.com>
2024-06-17 11:56:43 +0530

    chore: Update meta tags and page title in header.php
    The meta tags and page title in the header.php file have been updated to improve SEO and provide more accurate information about the website. This includes adding relevant keywords, setting appropriate meta descriptions, and specifying Open Graph (og) meta tags for better social media sharing. Additionally, the page title has been changed to "Best Tour Operator in Ahmedabad, Gujarat" to better reflect the website's purpose.

--- _header.php
+++ _header.php
@@ -3,8 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Krish Panchani, Bhargav Asodariya and Thunder Develops">
+
+    <!-- SEO meta tags -->
+    <meta name="description" content="Looking for a reliable and affordable tour operator in Ahmedabad? Look no further than Gujarat Darshan! We offer a wide range of tour packages that are perfect for both leisure and business travelers.">
+    <meta name="keywords" content="tour operator, Ahmedabad, Gujarat, travel, tour packages">
+    <meta name="robots" content="index, follow">
+    <meta name="rating" content="general">
+    <meta name="distribution" content="global">
+    <meta name="revisit-after" content="7 days">
+    <meta name="language" content="en">
+
+    <!-- Open Graph (og) meta tags -->
+    <meta property="og:title" content="Best Tour Operator in Ahmedabad, Gujarat">
+    <meta property="og:description" content="Looking for a reliable and affordable tour operator in Ahmedabad? Look no further than Gujarat Darshan! We offer a wide range of tour packages that are perfect for both leisure and business travelers.">
+    <meta property="og:image" content="your-image-url.jpg">
+    <meta property="og:url" content="https://your-website-url.com">
+    <meta property="og:type" content="website">
+
     <link rel="stylesheet" href="css/output.css">
     <!-- <link href="https://unpkg.com/tailwindcss@1.2.0/dist/tailwind.min.css" rel="stylesheet"> -->
-    <title>Document</title>
+    <title>Best Tour Operator in Ahmedabad, Gujarat</title>
+
+    <link rel="stylesheet" href="/css/index.css">
+    <script src="/js/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+      <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
 <body>
