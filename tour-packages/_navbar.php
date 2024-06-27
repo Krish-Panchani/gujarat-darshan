4905124f74dc290b0fe792dfa6ba7876ef01b6e6
Krish-Panchani <krishpanchani1346@gmail.com>
2024-06-27 14:23:38 +0530

    chore: Update navbar layout and styles

--- tour-packages/_navbar.php
+++ tour-packages/_navbar.php
@@ -1,11 +1,11 @@
 <?php 
-$linkClass = "px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
-$dropdownLinkClass = "block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
+$linkClass = " px-4 py-2 mt-2 text-base font-bold bg-white rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
+$dropdownLinkClass = "z-50 block px-4 py-2 mt-2 text-sm font-semibold bg-white rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline";
 
 ?>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-<div class="w-full text-gray-700 bg-white">
-    <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+<div class="w-full text-gray-700 bg-white px-2">
+    <div x-data="{ open: false }" class="flex flex-col max-w-screen-2xl px-4 mx-auto border-0 rounded-2xl md:border-b-2  border-black my-4 md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div class="p-4 flex flex-row items-center justify-between">
             <a href="#" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline">
                 <img class="w-40" src="logo.png" alt="Logo">
@@ -21,72 +21,72 @@ $dropdownLinkClass = "block px-4 py-2 mt-2 text-sm font-semibold bg-transparent 
             <a class="<?php echo $linkClass;?>" href="#">Home</a>
             <div @click.away="open = false" class="relative" x-data="{ open: false }">
                 <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                    <span>Tour packages</span>
-                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                    <span class="text-base font-bold">Tour packages</span>
+                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-5 h-5 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                    <div class="px-2 py-2 bg-white rounded-md shadow">
-                        <a class="block <?php echo $linkClass;?>" href="#">Rann of kutch package</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Dwarka tour packages</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">somnath tour packages</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">dwarka somnath tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">pancha dwarka yatra</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">statue of unity tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">saurashtra tour ahmedabad</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">gir jungle safari</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">gujarat diu tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">best of kutch tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">colours of gujarat tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">gujarat desert beach tour</a>
+                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-60 z-50">
+                    <div class="z-50 px-2 py-2 bg-white rounded-md shadow">
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Rann of kutch package</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Dwarka tour packages</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">somnath tour packages</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">dwarka somnath tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">pancha dwarka yatra</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">statue of unity tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">saurashtra tour ahmedabad</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">gir jungle safari</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">gujarat diu tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">best of kutch tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">colours of gujarat tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">gujarat desert beach tour</a>
                     </div>
                 </div>
             </div>
             <div @click.away="open = false" class="relative" x-data="{ open: false }">
                 <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                    <span>Regular tour</span>
-                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                    <span class="text-base font-bold">Regular tour</span>
+                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-5 h-5 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                    <div class="px-2 py-2 bg-white rounded-md shadow">
-                        <a class="block <?php echo $linkClass;?>" href="#">2 Days - Statue of Unity Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">3 Days - Dwarka Somnath Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">3 Days - Dwarka Somnath with SOU</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">4 Days - Dwarka Somnath Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">4 Days - Saurashtra Tour with Porbandar</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">5 Days - Dwarka Somnath Diu & Gir Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">6 Days - Dwarka Somnath Diu & Gir Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">7 Days - Dwarka Somnath Diu & Gir Tour</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">8 Days - Dwarka Somnath with Bhuj</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">9 Days - Saurashtra with SOU & Kytch</a>
+                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-72 z-50">
+                    <div class="z-50 px-2 py-2 bg-white rounded-md shadow">
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">2 Days - Statue of Unity Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">3 Days - Dwarka Somnath Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">3 Days - Dwarka Somnath with SOU</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">4 Days - Dwarka Somnath Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">4 Days - Saurashtra Tour with Porbandar</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">5 Days - Dwarka Somnath Diu & Gir Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">6 Days - Dwarka Somnath Diu & Gir Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">7 Days - Dwarka Somnath Diu & Gir Tour</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">8 Days - Dwarka Somnath with Bhuj</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">9 Days - Saurashtra with SOU & Kytch</a>
                     </div>
                 </div>
             </div>
             <div @click.away="open = false" class="relative" x-data="{ open: false }">
                 <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                    <span>Hotels</span>
-                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                    <span class="text-base font-bold">Hotels</span>
+                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-5 h-5 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                    <div class="px-2 py-2 bg-white rounded-md shadow">
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Somnath</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Dwarka</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Ahmedabad</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Gir</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Diu</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Rajkot</a>
-                        <a class="block <?php echo $linkClass;?>" href="#">Hotels in Rann of Kutch</a>
+                <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute bg-white right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-60 z-50">
+                    <div class="z-50 px-2 py-2 bg-white rounded-md shadow ">
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="Hotels/Hotels-in-somnath">Hotels in Somnath</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Dwarka</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Ahmedabad</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Gir</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Diu</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Rajkot</a>
+                        <a class="block <?php echo $dropdownLinkClass;?>" href="#">Hotels in Rann of Kutch</a>
                     </div>
                 </div>
             </div>
             
-                <a class="<?php echo $linkClass;?>" href="#">About</a>
-                <a class="<?php echo $linkClass;?>" href="#">Contact</a>
+            <a class="<?php echo $linkClass;?>" href="#">About</a>
+            <a class="<?php echo $linkClass;?>" href="#">Contact</a>
         </nav>
     </div>
 </div>
