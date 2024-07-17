3101aadbb91781f000f793beba471e864a10b26b
Krish-Panchani <krishpanchani1346@gmail.com>
2024-07-17 17:34:05 +0530

    chore: Update footer link to "Gujarat Darshan"

--- _footer.php
+++ _footer.php
@@ -54,7 +54,7 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-lg text-gray-500 sm:text-center">© <?php echo date("Y"); ?> <a href="/" class="text-red-500 font-semibold hover:underline">Gujarat Travels</a>. All Rights Reserved.</span>
+            <span class="text-lg text-gray-500 sm:text-center">© <?php echo date("Y"); ?> <a href="/" class="text-red-500 font-semibold hover:underline">Gujarat Darshan</a>. All Rights Reserved.</span>
             </span>
             <div class="flex mt-4 sm:justify-center sm:mt-0">
                 <a href="#" class="text-gray-500 hover:text-gray-900">
