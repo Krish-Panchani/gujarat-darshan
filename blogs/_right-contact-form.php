e2cc5c4992d8c521088c781b94a0252b07c6b4d5
Krish-Panchani <krishpanchani1346@gmail.com>
2024-07-27 12:14:36 +0530

    Update header and contact form for blogs page

---
+++ blogs/_right-contact-form.php
@@ -0,0 +1,42 @@
+<div>
+    <div class="mt-12 mx-auto px-4 p-8 bg-white border max-w-lg border-gray-700 sm:px-8 sm:rounded-xl">
+        <h3 class="text-red-400 font-semibold">
+            Contact Us
+        </h3>
+        <p class="text-black text-2xl font-bold sm:text-3xl pb-6">
+            Get Free Quote
+        </p>
+        <form onsubmit="event.preventDefault()" class="space-y-5">
+            <div>
+                <label class="font-medium">
+                    Full name
+                </label>
+                <input type="text" required class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg" />
+            </div>
+            <div>
+                <label class="font-medium">
+                    Email
+                </label>
+                <input type="email" required class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg" />
+            </div>
+            <div>
+                <label class="font-medium">
+                    Phone number
+                </label>
+                <div class="relative mt-2">
+                    <input type="text" placeholder="+1 (555) 000-000" required class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg" />
+                </div>
+            </div>
+            <div>
+                <label class="font-medium">
+                    Message
+                </label>
+                <textarea required class="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"></textarea>
+            </div>
+            <button class="w-full px-4 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150">
+                Submit
+            </button>
+        </form>
+    </div>
+
+</div>
