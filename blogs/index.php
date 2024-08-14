106f508bbc2bdc29ac0ffa994b303108d65e188d
Krish-Panchani <krishpanchani1346@gmail.com>
2024-08-14 15:07:32 +0530

    refactor: Update image path for car hire service

--- blogs/index.php
+++ blogs/index.php
@@ -1,38 +1,10 @@
-<article id="post-969" class="single-post-dv post-969 post type-post status-publish format-standard has-post-thumbnail hentry category-travel-guide">
+<?php
+$title = "Taxi, Cab Hire in Ahmedabad News and Blog | Gujarat Darshan";
+$desc = "Taxi, Cab Hire in Ahmedabad News and Blog: Latest news and blog posts about taxi and cab services in Ahmedabad, India.";
+require_once "_header.php"; ?>
+<?php require_once "_navbar.php"; ?>
 
-    <div class="post-thumbnail">
-        <img width="1200" height="628" src="https://www.gujaratdarshans.com/wp-content/uploads/2024/04/best-places-visit-gandhinagar.png" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="Best Places to Visit in Gandhinagar" decoding="async" fetchpriority="high" srcset="https://www.gujaratdarshans.com/wp-content/uploads/2024/04/best-places-visit-gandhinagar.png 1200w, https://www.gujaratdarshans.com/wp-content/uploads/2024/04/best-places-visit-gandhinagar-300x157.png 300w, https://www.gujaratdarshans.com/wp-content/uploads/2024/04/best-places-visit-gandhinagar-1024x536.png 1024w, https://www.gujaratdarshans.com/wp-content/uploads/2024/04/best-places-visit-gandhinagar-768x402.png 768w" sizes="(max-width: 1200px) 100vw, 1200px">
-    </div><!-- .post-thumbnail -->
+<?php require_once "_footer.php"; ?>
+    </body>
 
-    <span class="date_blk">April 25, 2024</span>
-
-    <div class="entry-content">
-
-        <p>Gandhinagar, the capital of Gujarat and a city on the banks of the Sabarmati River is a popular tourist destination in India. Gandhinagar, in stark contrast to Ahmedabad, is a tranquil town full of picturesque landscapes, fascinating attractions, and thrilling activities.</p>
-        <p>Gardens, wide avenues lined with trees, stunning buildings, temples, marketplaces, and a plethora of other amenities make up this city, which was named after the “Father of the Nation,” Mahatma Gandhi. Actually, it’s known for being one of the country’s most well-designed urban areas. Here are some top tourist attractions near Gandhinagar.</p>
-        <h2 class="wp-block-heading">5 Best places to visit in Gandhinagar</h2>
-        <h3 class="wp-block-heading"><a></a>Adalaj Stepwell</h3>
-        <p>In 1498, the Adalaj Stepwell was constructed as one of numerous step wells in India that were used to obtain groundwater. The building as a whole is a testament to the high level of engineering and architectural acumen prevalent in ancient India. The Adalaj Stepwell is an impressive man-made structure that was skillfully constructed to alleviate the water shortage in and around Adalaj Village. Approximately three or four kilometers southwest of Gandhinagar, the capital city of Gujarat, lies the stepwell. </p>
-        <p>In 1498, the Adalaj Stepwell was constructed as one of numerous step wells in India that were used to obtain groundwater. The building as a whole is a testament to the high level of engineering and architectural acumen prevalent in ancient India. The Adalaj Stepwell is an impressive man-made structure that was skillfully constructed to alleviate the water shortage in and around Adalaj Village. Approximately three or four kilometers southwest of Gandhinagar, the capital city of Gujarat, lies the stepwell.</p>
-        <p class="has-text-align-center">Also Read: <a href="https://www.gujaratdarshans.com/visit-ahmedabad-iconic-patang-hotel/">Visit Ahmedabad’s iconic Patang Hotel</a></p>
-        <h3 class="wp-block-heading"><a></a>Sarita Udyan</h3>
-        <p>Locals love to picnic in Sarita Udyan, and it’s conveniently situated next to a deer park, another must-visit attraction. It captivates audiences of all ages, from children to adults.</p>
-        <h3 class="wp-block-heading"><a></a>Craftsmen’s Village</h3>
-        <p>Hundreds of tourists and shoppers flock to Gandhinagar’s Craftsmen’s Village every day, which is known for its Bandhani sarees and is situated beside the Sabarmati river. Using vibrantly colored wooden printing blocks, artisans hand-print sarees and garments. The clothes are inexpensive and expertly made here.</p>
-
-        <h3 class="wp-block-heading"><a></a>Indroda Dinosaur &amp; Fossil Park</h3>
-
-        <p>The Indroda Dinosaur &amp; Fossil Park is a magnificent 400-hectare site that lies on the banks of the River Sabarmati. The park is home to the skeletons of massive creatures like blue whales, and it is also the world’s second-largest dinosaur-egg hatchery! In addition to the camping facilities, the park’s grounds have a large botanical garden, an amphitheater, and an interpretation center.</p>
-
-        <h3 class="wp-block-heading"><a></a>Sant Sarovar Dam</h3>
-
-        <p>The Sant Sarovar Dam, constructed to divert the dangerous currents of the River Sabarmati, is a popular Gandhinagar picnic spot. Indulge in a refreshing plunge in the sacred river and have a memorable weekend with your loved ones at this popular spot, which is conveniently located near the Sarita Garden. During the monsoons, be cautious since the ground can become very wet and the currents can be quite violent.</p>
-
-        <p class="has-text-align-center">Also Read: <a href="https://www.gujaratdarshans.com/nadabet-seema-darshan-tour/">Nadabet Seema Darshan Tour Complete Guide</a></p>
-
-        <h3 class="wp-block-heading"><a></a>Conclusion</h3>
-        <p>Is the prospect of visiting one of these amazing Gandhinagar attractions already filling you with excitement? With its many historical sites and breathtaking vistas, this capital city is a must-visit for history buffs. If you are considering a trip to Gujarat in the near future, you should definitely not miss out on these wonderful and worthwhile sites.</p>
-    </div><!-- .entry-content -->
-
-    <!-- .entry-footer -->
-</article>
+    </html>
