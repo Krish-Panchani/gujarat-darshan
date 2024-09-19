04ea289872fd0fe01f2bc97061b592b89ad8babf
Krish-Panchani <krishpanchani1346@gmail.com>
2024-09-19 16:17:06 +0530

    feat: Add inquiry button to contact section and reposition contact form modal

--- index.php
+++ index.php
@@ -6,12 +6,6 @@ require_once "_header.php"; ?>
 <?php require_once "_navbar.php"; ?>
 <?php require_once "_whatsapp_btn.php"; ?>
 
-    <!-- Button to trigger the modal -->
-    <button class="openModalBtn px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 transition">
-    Open Form
-</button>
-
-    <?php require '_contact-form-popup.php'; ?>
 
 <div class="w-full relative px-6">
   <div class="swiper vertical-slide-carousel swiper-container relative h-96 rounded-3xl">
@@ -19,9 +13,14 @@ require_once "_header.php"; ?>
       <div class="bg1 swiper-slide ">
         <div class="slide-div h-96 flex justify-center items-center flex-col">
           <h3 class="swiper-text px-4 text-3xl sm:text-6xl font-bold text-white">Welcome to Gujarat Darshan</h3>
-          <a href="/contact-us">
-            <button class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gradient-to-r from-rose-400 to-red-500 rounded-lg hover:bg-red-400 mt-8">Explore Now</button>
-          </a>
+          <div class="flex justify-center items-center gap-6">
+            <a href="/contact-us">
+              <button class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gradient-to-r from-rose-400 to-red-500 rounded-lg hover:bg-red-400 mt-8">Explore Now</button>
+            </a>
+            <button class="openModalBtn px-6 py-2 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-800 transition">
+              Inquire Now
+            </button>
+          </div>
         </div>
       </div>
       <div class="bg2 swiper-slide">
@@ -52,6 +51,8 @@ require_once "_header.php"; ?>
     <div class="swiper-pagination text-orange-500"></div>
   </div>
 </div>
+<?php require '_contact-form-popup.php'; ?>
+
 
 <section class="py-8">
   <div class="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
@@ -182,64 +183,64 @@ require_once "_header.php"; ?>
 </section>
 
 <section class="relative py-28 bg-gray-900">
-    <div class="relative z-10 max-w-screen-xl mx-auto px-4 text-gray-300 justify-between gap-24 lg:flex md:px-8">
-        <div class="max-w-xl">
-            <h3 class="text-white text-3xl font-semibold sm:text-4xl">
-                Let us show you the Gujarat
-            </h3>
-            <p class="mt-3">
-                Let us show you the Gujarat that few know of – its rich tradition, diverse culture, delectable cuisine and its scintillating crafts.
-            </p>
-        </div>
-        <div class="mt-12 lg:mt-0">
-            <ul class="grid gap-8 sm:grid-cols-2">
-                <?php
-                $features = [
-                    [
-                        'icon' => '<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
-                ',
-                        'title' => 'Reasonable Price',
-                        'desc' => 'We offer one of the most reasonable price throughout the market.'
-                    ],
-                    [
-                        'icon' => '<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-              ',
-                        'title' => '20+ Years of Experience',
-                        'desc' => 'We are very proud to say that we can give you the best advice for your trip.'
-                    ],
-                    [
-                        'icon' => '<path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                ',
-                        'title' => 'Professional Local Tour Guide',
-                        'desc' => 'You will be full of information with our exceptional local tour guide.'
-                    ],
-                ];
-
-                foreach ($features as $feature) {
-                ?>
-                    <li class="flex gap-x-4">
-                        <div class="flex-none w-12 h-12 bg-gray-700 text-red-400 rounded-lg flex items-center justify-center">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <?php echo $feature['icon']; ?>
-                </svg>
-                        </div>
-                        <div>
-                            <h4 class="text-lg text-gray-100 font-semibold">
-                                <?php echo $feature['title']; ?>
-                            </h4>
-                            <p class="mt-3">
-                                <?php echo $feature['desc']; ?>
-                            </p>
-                        </div>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
-        </div>
+  <div class="relative z-10 max-w-screen-xl mx-auto px-4 text-gray-300 justify-between gap-24 lg:flex md:px-8">
+    <div class="max-w-xl">
+      <h3 class="text-white text-3xl font-semibold sm:text-4xl">
+        Let us show you the Gujarat
+      </h3>
+      <p class="mt-3">
+        Let us show you the Gujarat that few know of – its rich tradition, diverse culture, delectable cuisine and its scintillating crafts.
+      </p>
     </div>
-    <div class="absolute inset-0 max-w-md mx-auto h-72 blur-[118px]" style="background: linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)"></div>
+    <div class="mt-12 lg:mt-0">
+      <ul class="grid gap-8 sm:grid-cols-2">
+        <?php
+        $features = [
+          [
+            'icon' => '<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+                ',
+            'title' => 'Reasonable Price',
+            'desc' => 'We offer one of the most reasonable price throughout the market.'
+          ],
+          [
+            'icon' => '<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+              ',
+            'title' => '20+ Years of Experience',
+            'desc' => 'We are very proud to say that we can give you the best advice for your trip.'
+          ],
+          [
+            'icon' => '<path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                ',
+            'title' => 'Professional Local Tour Guide',
+            'desc' => 'You will be full of information with our exceptional local tour guide.'
+          ],
+        ];
+
+        foreach ($features as $feature) {
+        ?>
+          <li class="flex gap-x-4">
+            <div class="flex-none w-12 h-12 bg-gray-700 text-red-400 rounded-lg flex items-center justify-center">
+
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <?php echo $feature['icon']; ?>
+              </svg>
+            </div>
+            <div>
+              <h4 class="text-lg text-gray-100 font-semibold">
+                <?php echo $feature['title']; ?>
+              </h4>
+              <p class="mt-3">
+                <?php echo $feature['desc']; ?>
+              </p>
+            </div>
+          </li>
+        <?php
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+  <div class="absolute inset-0 max-w-md mx-auto h-72 blur-[118px]" style="background: linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)"></div>
 </section>
 
 
@@ -421,27 +422,27 @@ require_once "_header.php"; ?>
 </script>
 
 <script>
-        // Get modal, button, and close elements
-        const modal = document.getElementById('myModal');
-        const openModalBtn = document.getElementById('openModal');
-        const closeModalBtn = document.getElementById('closeModal');
+  // Get modal, button, and close elements
+  const modal = document.getElementById('myModal');
+  const openModalBtn = document.getElementById('openModal');
+  const closeModalBtn = document.getElementById('closeModal');
 
-        // When the user clicks the button, open the modal
-        openModalBtn.addEventListener('click', () => {
-            modal.classList.remove('hidden');
-        });
+  // When the user clicks the button, open the modal
+  openModalBtn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+  });
 
-        // When the user clicks the "close" button, hide the modal
-        closeModalBtn.addEventListener('click', () => {
-            modal.classList.add('hidden');
-        });
+  // When the user clicks the "close" button, hide the modal
+  closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('hidden');
+  });
 
-        // Close the modal if user clicks outside of the modal content
-        window.addEventListener('click', (event) => {
-            if (event.target == modal) {
-                modal.classList.add('hidden');
-            }
-        });
-    </script>
+  // Close the modal if user clicks outside of the modal content
+  window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+      modal.classList.add('hidden');
+    }
+  });
+</script>
 
 <?php require_once "_footer.php"; ?>
